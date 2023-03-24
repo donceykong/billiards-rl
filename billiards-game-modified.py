b6ac34977498a0e3a0124f4b236eff5e02542748
Doncey Albin <donceyalbin@gmail.com>
2023-03-24 12:22:16 -0600

    Added punch sound to impact audio sounds

--- billiards-game-modified.py
+++ billiards-game-modified.py
@@ -12,7 +12,8 @@ import pygame
 import pymunk
 import pymunk.pygame_util
 import math
-
+import numpy as np
+import random
 pygame.init()
 
 SCREEN_WIDTH = 1200
@@ -30,7 +31,7 @@ draw_options = pymunk.pygame_util.DrawOptions(screen)
 
 #clock
 clock = pygame.time.Clock()
-FPS = 120
+FPS = 500
 
 #game variables
 lives = 1        # Number of times cue ball can be potted
@@ -56,7 +57,8 @@ large_font = pygame.font.SysFont("Lato", 60)
 
 #load images
 cue_image = pygame.image.load("assets/images/cue.png").convert_alpha()
-arrow_image = pygame.image.load("assets/images/arrow_half.png").convert_alpha()
+arrow_image_midpoint = pygame.image.load("assets/images/arrow_half.png").convert_alpha()
+arrow_image_endpoint = pygame.image.load("assets/images/arrow.png").convert_alpha()
 table_image = pygame.image.load("assets/images/table.png").convert_alpha()
 
 # Load images of balls
@@ -77,6 +79,19 @@ def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
+def create_aim_boundary(radius, pos):
+  body = pymunk.Body(body_type = pymunk.Body.STATIC)
+  body.position = pos
+  shape = pymunk.Circle(body, radius)
+  shape.mass = 0
+  shape.elasticity = 0.8
+  #use pivot joint to add friction
+  pivot = pymunk.PivotJoint(static_body, body, (0, 0), (0, 0))
+  pivot.max_bias = 0.0 # disable joint correction
+  pivot.max_force = 0 # emulate linear friction
+  space.add(body, shape)
+  return shape
+
 #function for creating balls
 def create_ball(radius, pos):
   body = pymunk.Body()
@@ -86,7 +101,7 @@ def create_ball(radius, pos):
   shape.elasticity = 0.8
   #use pivot joint to add friction
   pivot = pymunk.PivotJoint(static_body, body, (0, 0), (0, 0))
-  pivot.max_bias = 0 # disable joint correction
+  pivot.max_bias = 0.0 # disable joint correction
   pivot.max_force = 1000 # emulate linear friction
 
   space.add(body, shape, pivot)
@@ -142,7 +157,6 @@ def create_cushion(poly_dims):
   body.position = ((0, 0))
   shape = pymunk.Poly(body, poly_dims)
   shape.elasticity = 0.8
-  
   space.add(body, shape)
 
 for c in cushions:
@@ -150,8 +164,11 @@ for c in cushions:
 
 #create VectorArrow
 class VectorArrow():
-  def __init__(self, cue_ball_pos):
-    self.original_image = arrow_image
+  def __init__(self, cue_ball_pos, vector_type = "midpoint"):
+    if vector_type == "midpoint":
+      self.original_image = arrow_image_midpoint
+    elif type == "endpoint":
+      self.original_image = arrow_image_endpoint
     self.angle = 0
     self.image = pygame.transform.rotate(self.original_image, self.angle)
     self.rect = self.image.get_rect()
@@ -166,6 +183,17 @@ class VectorArrow():
     # rotate wrt self
     self.image = pygame.transform.rotate(self.image, self.angle)
 
+    #self.rect = self.image.get_rect()
+    #pygame.draw.rect(screen, BG, self.rect)
+    for c in cushions:
+      pygame.draw.polygon(screen, pygame.color.Color(0, 0, 255), c)
+
+    for ball in balls:
+      pygame.draw.circle(screen, pygame.color.Color(0, 0, 255), ball.body.position, dia / 2)
+
+    for pocket in pockets:
+      pygame.draw.circle(screen, pygame.color.Color(0, 0, 255), pocket, pocket_dia / 2)
+
     surface.blit(
       self.image,
       (self.rect.centerx - self.image.get_width() / 2,
@@ -173,7 +201,9 @@ class VectorArrow():
       )
 
 vector_arrow = VectorArrow(balls[-1].body.position)
-vector_arrow2 = VectorArrow(balls[-1].body.position)
+loa_arrow = VectorArrow(balls[-1].body.position)
+ray2_arrow = VectorArrow(balls[-2].body.position)
+nearest_pocket_arrow = VectorArrow(balls[-2].body.position)
 
 #create pool cue
 class Cue():
@@ -189,6 +219,7 @@ class Cue():
 
   def draw(self, surface):
     self.image = pygame.transform.rotate(self.original_image, self.angle)
+
     surface.blit(self.image,
       (self.rect.centerx - self.image.get_width() / 2, self.rect.centery - self.image.get_height() / 2)
      )
@@ -199,10 +230,14 @@ cue = Cue(balls[-1].body.position)
 power_bar = pygame.Surface((10, 20))
 power_bar.fill(RED)
 
+justplayedimpactsound = True
+
+winners = ["assets/audio/winner/winner1.mp3"]
+losers = ["assets/audio/loser/loser1.mp3", "assets/audio/loser/loser2.mp3"]
+
 #game loop
 run = True
 while run:
-
   clock.tick(FPS)
   space.step(1 / FPS)
 
@@ -225,16 +260,15 @@ while run:
           cue_ball_potted = True
           ball.body.position = (-100, -100)
           ball.body.velocity = (0.0, 0.0)
+          pygame.mixer.music.load(f"assets/audio/loser/loser{random.randint(1, 6)}.mp3")
+          pygame.mixer.music.play(0)
         else:
           space.remove(ball.body)
           balls.remove(ball)
           potted_balls.append(ball_images[i])
           ball_images.pop(i)
-
-  if len(balls) > 1:
-    target_ball_y_pos = int(balls[len(balls) - 2].body.position[1])
-    target_ball_y_pos = int(balls[len(balls) - 2].body.position[1])
-
+          pygame.mixer.music.load(f"assets/audio/winner/winner{random.randint(1, 2)}.mp3")
+          pygame.mixer.music.play(0)
 
   #draw pool balls
   for i, ball in enumerate(balls):
@@ -248,49 +282,107 @@ while run:
 
   #draw pool cue
   if taking_shot == True and game_running == True:
+    space.remove(balls[0].body)
+    aim_boundary = create_aim_boundary(dia, balls[0].body.position)
+
     if cue_ball_potted == True:
       #reposition cue ball
       balls[-1].body.position = (888, SCREEN_HEIGHT / 2)
       cue_ball_potted = False
 
-    #calculate pool cue angle
+    # Get positions of cursor, cue ball, and target ball
     mouse_pos = pygame.mouse.get_pos()
+    #print(f"mouse_pos: {mouse_pos}")
+    cue_ball_position = balls[-1].body.position
+    target_ball_index = 0 # Will leave as the first ball in list for now
+    target_ball_position = balls[target_ball_index].body.position
 
-    cue.rect.center = balls[-1].body.position
-    x_dist_cursor = balls[-1].body.position[0] - mouse_pos[0]
-    y_dist_cursor = -(balls[-1].body.position[1] - mouse_pos[1]) # -ve because pygame y coordinates increase down the screen
+    #calculate pool cue angle
+    cue.rect.center = cue_ball_position
+    x_dist_cursor = mouse_pos[0] - cue_ball_position[0]
+    y_dist_cursor = mouse_pos[1] - cue_ball_position[1] # -ve because pygame y coordinates increase down the screen
     cue_angle = math.degrees(math.atan2(y_dist_cursor, x_dist_cursor))
-    cue.update(cue_angle)
+    cue.update(-cue_angle)
     cue.draw(screen)
-
-    # Arrow to target ball
-    x_dist_target = balls[-1].body.position[0] - balls[-2].body.position[0]
-    y_dist_target = -(balls[-1].body.position[1] - balls[-2].body.position[1])
-    vector_angle = math.degrees(math.atan2(y_dist_target, x_dist_target))
-    vector_arrow.rect.center = balls[-1].body.position
-    new_arrow_width = 2 * math.sqrt(x_dist_target*x_dist_target + y_dist_target*y_dist_target)
-    vector_arrow.update(vector_angle)
-    vector_arrow.draw(screen, new_arrow_width)
 
     # Arrow to nearest pocket
     min_dist = 10000
     nearest_pocket = pockets[0]
     for pocket in pockets:
-      x_dist_pocket1 = balls[-2].body.position[0] - pocket[0]
-      y_dist_pocket1 = -(balls[-2].body.position[1] - pocket[1])
+      x_dist_pocket1 = target_ball_position[0] - pocket[0]
+      y_dist_pocket1 = -(target_ball_position[1] - pocket[1])
       euc_dist = math.sqrt(x_dist_pocket1*x_dist_pocket1 + y_dist_pocket1*y_dist_pocket1)
-
       if euc_dist < min_dist:
         min_dist = euc_dist
         nearest_pocket = pocket
 
-    x_dist_pocket1 = balls[-2].body.position[0] - nearest_pocket[0]
-    y_dist_pocket1 = -(balls[-2].body.position[1] - nearest_pocket[1])
+    x_dist_pocket1 = target_ball_position[0] - nearest_pocket[0]
+    y_dist_pocket1 = -(target_ball_position[1] - nearest_pocket[1])
     vector_angle = math.degrees(math.atan2(y_dist_pocket1, x_dist_pocket1))
-    vector_arrow2.rect.center = balls[-2].body.position
+    nearest_pocket_arrow.rect.center = target_ball_position
     new_arrow_width = 2 * math.sqrt(x_dist_pocket1*x_dist_pocket1 + y_dist_pocket1*y_dist_pocket1)
-    vector_arrow2.update(vector_angle)
-    vector_arrow2.draw(screen, new_arrow_width)
+    nearest_pocket_arrow.update(vector_angle)
+    nearest_pocket_arrow.draw(screen, new_arrow_width)
+
+    # Check if LOA intersects target ball
+    '''
+    returns:
+    - shape:  Shape that was hit, or None if no collision occured
+    - point:  The point of impact.
+    - normal: The normal of the surface hit.
+    - alpha:  The normalized distance along the query segment in the range [0, 1]
+    '''
+    loa_angle = math.degrees(math.atan2(-y_dist_cursor, -x_dist_cursor)) # will be in the opp direction (mouse to cue ball)
+    loa_angle_rad = np.deg2rad(loa_angle)
+    loa_arrow.rect.center = [cue_ball_position[0] + dia*np.cos(loa_angle_rad), 
+                             cue_ball_position[1] + dia*np.sin(loa_angle_rad)]
+    
+    loa_length = 2000
+    start = loa_arrow.rect.center
+    end = [start[0] + loa_length*np.cos(loa_angle_rad),
+          start[1] + loa_length*np.sin(loa_angle_rad)]
+    radius = 0.00001 # the radius of the object that is raycasted?
+    filter = pymunk.ShapeFilter(mask=pymunk.ShapeFilter.ALL_MASKS)
+    raycast_info = space.segment_query_first(start=start, end=end, radius=radius, shape_filter=pymunk.ShapeFilter())
+    if raycast_info != None:
+      x_dist_loa = raycast_info.point[0] - start[0]
+      y_dist_loa = raycast_info.point[1] - start[1]
+    else:
+      x_dist_loa = 2
+      y_dist_loa = 2
+    loa_arrow.update(-cue_angle)
+    new_arrow_width = 2 * math.sqrt(x_dist_loa*x_dist_loa + y_dist_loa*y_dist_loa)
+    loa_arrow.draw(screen, new_arrow_width)
+
+    # Raycast 2
+    ray2_arrow_width = 100
+    if (raycast_info != None and type(raycast_info.shape) == pymunk.shapes.Circle):
+      ray2_angle = math.degrees(np.arctan2(raycast_info.normal[1],raycast_info.normal[0]))
+      ray2_angle_rad = np.deg2rad(ray2_angle)
+      ray2_start = [raycast_info.point[0] - 2*dia*1.5*raycast_info.normal[0],
+                    raycast_info.point[1] - 2*dia*1.5*raycast_info.normal[1]]
+      ray2_arrow.rect.center = ray2_start
+      ray2_end = [ray2_start[0] - loa_length*raycast_info.normal[0],
+                  ray2_start[1] - loa_length*raycast_info.normal[1]]
+      radius = 0.01 # the radius of the object that is raycasted
+      filter = pymunk.ShapeFilter(mask=pymunk.ShapeFilter.ALL_MASKS)
+      raycast_info = space.segment_query_first(start=ray2_start, end=ray2_end, radius=radius, shape_filter=pymunk.ShapeFilter())
+      if raycast_info != None:
+        ray2_x_dist = raycast_info.point[0] - ray2_start[0]
+        ray2_y_dist = raycast_info.point[1] - ray2_start[1]
+      else:
+        if abs(ray2_angle+vector_angle) < 0.01:
+          powering_up == False and taking_shot == True
+          active_force = 10000
+        ray2_x_dist = 2000
+        ray2_y_dist = 2000
+      #print(f"ray2_angle: {ray2_angle}, vector_angle: {vector_angle}")
+      ray2_arrow.update(-ray2_angle)
+      ray2_arrow_width = 2 * math.sqrt(ray2_x_dist*ray2_x_dist + ray2_y_dist*ray2_y_dist)
+      ray2_arrow.draw(screen, ray2_arrow_width)
+    
+    space.remove(aim_boundary)
+    space.add(balls[0].body) 
 
   #power up pool cue
   if powering_up == True and game_running == True:
@@ -303,8 +395,8 @@ while run:
        (balls[-1].body.position[0] - 30 + (b * 15),
         balls[-1].body.position[1] + 30))
   elif powering_up == False and taking_shot == True:
-    x_impulse = math.cos(math.radians(cue_angle))
-    y_impulse = math.sin(math.radians(cue_angle))
+    x_impulse = math.cos(math.radians(-cue_angle))
+    y_impulse = math.sin(math.radians(-cue_angle))
     balls[-1].body.apply_impulse_at_local_point((active_force * -x_impulse, active_force * y_impulse), (0, 0))
     active_force = 0
     force_direction = 1
