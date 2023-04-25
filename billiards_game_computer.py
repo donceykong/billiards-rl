76dcddd056039723ad5573cecd075da108266ce5
Doncey Albin <donceyalbin@gmail.com>
2023-04-25 12:14:12 -0600

    updated billiards game for computer

--- billiards_game_computer.py
+++ billiards_game_computer.py
@@ -283,9 +283,18 @@ class BILLIARDS_GAME_COMPUTER:
         self.reward = 100
       #check if all the balls have stopped moving
       sum_speed = 0.00
+      '''
+      POSSIBLE SOURCE OF ERROR.
+      '''
+      # TODO: Check to maybe set vel of each ball to zero if the registered speed is
+      # zero if it is close to zero.
       for i, ball in enumerate(self.balls):
-        sum_speed += np.linalg.norm(ball.body.velocity[0] - ball.body.velocity[1])
-      if sum_speed != 0:
+        ball_speed = np.linalg.norm(ball.body.velocity[0] - ball.body.velocity[1])
+      #  print(f"ball_speed: {ball_speed}") 
+        # if ball_speed < ___: ball.body.velocity = [0.00, 0.00]
+        sum_speed += ball_speed
+      #print(f"sum_speed: {sum_speed}") 
+      if sum_speed > 1e-30:
         has_velocity = True
       else:
         has_velocity = False
